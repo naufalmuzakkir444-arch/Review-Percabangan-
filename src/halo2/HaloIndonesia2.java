7416ffa6b2d6d939c559f0a313891909718b0fe6
naufalmuzakkir444-arch <naufalmuzakkir444@gmail.com>
2025-09-14 06:53:57 +0800

    Commit Kedua

---
+++ src/halo2/HaloIndonesia2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halo2;
+
+/**
+ *
+ * @author USER MSI
+ */
+public class HaloIndonesia2 {
+    public static void main(String[] args) {
+        System.out.println("Halo Indonesia 2");
+        System.out.println("Selamat Pagi Indonesia 2");
+        System.out.println("Jaya Indonesia 2");   
+    }
+}
---
+++ src/salam2/Ucapan2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salam2;
+
+/**
+ *
+ * @author USER MSI
+ */
+public class Ucapan2 {
+    public static void main(String[] args) {
+        System.out.println("Halo ULM 2");
+        System.out.println("ULM Berdaya Saing dan Unggul di Bidang Lahan Basah 2");
+        System.out.println("Prodi Ilkom ULM Unggul dengan Teknologi Kekinian 2");
+    }
+}
