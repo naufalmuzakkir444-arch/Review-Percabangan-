e2cd6e301500d37da4a091ec47833a38fa7c0a71
MUHAMMAD NAUFAL MUZAKKIR <naufalmuzakkir444@gmail.com>
2025-09-14 19:59:18 +0800

    Delete HaloIndonesia.java

--- HaloIndonesia.java
+++
@@ -1,7 +0,0 @@
-public class HaloIndonesia2 {
-    public static void main(String[] args) {
-        System.out.println("Halo Indonesia ");
-        System.out.println("Selamat Pagi Indonesia ");
-        System.out.println("Jaya Indonesia ");   
-    }
-}
