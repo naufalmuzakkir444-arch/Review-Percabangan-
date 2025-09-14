32274b2cfa5de0ec4dfe2db7a84739a00a10033b
MUHAMMAD NAUFAL MUZAKKIR <naufalmuzakkir444@gmail.com>
2025-09-14 19:58:39 +0800

    Delete Ucapan.java

--- Ucapan.java
+++
@@ -1,7 +0,0 @@
-public class Ucapan {
-    public static void main(String[] args) { 
-      System.out.println("Halo ULM");
-      System.out.println("ULM Berdaya Saing dan Unggul di Bidang Lahan Basah");
-      System.out.println("Prodi Ilkom ULM Unggul dengan Teknologi Kekinian");
-    }
-}
